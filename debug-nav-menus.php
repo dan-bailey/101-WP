7118190d4cd1f5c9ac142d602fd7334a11fdca3b
Dan Bailey <dbailey@danbailey.net>
2026-06-02 20:52:08 -0500

    navmenu debug

---
+++ debug-nav-menus.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Debug script to check nav menu registration
+ *
+ * Usage: Add this to your theme's functions.php temporarily:
+ * add_action('admin_footer-nav-menus.php', function() {
+ *     $post_type = get_post_type_object('wp_101_list');
+ *     echo '<script>console.log("Post Type Object:", ' . json_encode($post_type) . ');</script>';
+ * });
+ */
+
+// This file is just documentation - see usage above
